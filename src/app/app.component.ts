1b8d051c9fe928166bd093546c439c4e7346a8ce
haltaction <haltaction@gmail.com>
2020-10-13 13:39:09 +0300

    Add bootstrap styles to front, clean-up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
+import {Component, ChangeDetectorRef, OnInit, OnDestroy} from '@angular/core';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState, FormFieldTypes } from '@aws-amplify/ui-components';
 import { Auth, API } from 'aws-amplify';
 
 @Component({
@@ -7,16 +7,26 @@ import { Auth, API } from 'aws-amplify';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'amplify-angular-auth';
   user: CognitoUserInterface | undefined;
   authState: AuthState;
 
   tasks: Array<{ id: string, name: string, any }> = [];
 
-  constructor(private ref: ChangeDetectorRef) {}
+  signUpFormFields: Partial<FormFieldTypes>;
 
-  async ngOnInit() {
+  isLoading = false;
+
+  constructor(private ref: ChangeDetectorRef) {
+    this.signUpFormFields = [
+      { type: 'username' },
+      { type: 'password' },
+      { type: 'email' }
+    ];
+  }
+
+  async ngOnInit(): Promise<void> {
     onAuthUIStateChange((authState, authData) => {
       this.authState = authState;
       this.user = authData as CognitoUserInterface;
@@ -25,35 +35,42 @@ export class AppComponent {
     await this.getTasks();
   }
 
+  // tslint:disable-next-line:typedef
   ngOnDestroy() {
     return onAuthUIStateChange;
   }
 
-  async addTask(element) {
+  async addTask(element): Promise<void> {
     const data = {
       body: {
         name: element.value
       }
     };
     element.value = '';
+    this.isLoading = true;
     await API.post('api', '/tasks', data);
     await this.getTasks();
+    this.isLoading = false;
   }
 
-  async getCredentials() {
+  async getCredentials(): Promise<void> {
     console.log(await Auth.currentSession());
     console.log(await Auth.currentCredentials());
   }
 
-  async getTasks() {
+  async getTasks(): Promise<void> {
+    this.isLoading = true;
     const apiData = await API.get('api', '/tasks', {});
     this.tasks = apiData.data.Items;
+    this.isLoading = false;
     this.ref.detectChanges();
   }
 
-  async deleteTask(taskId) {
+  async deleteTask(taskId): Promise<void> {
+    this.isLoading = true;
     await API.del('api', `/tasks/${taskId}`, {});
     await this.getTasks();
+    this.isLoading = false;
   }
 
 }
